add22987e0f5648a126bf9cd2f8c201a95fb9b57
JAMALFITANO <jona0610@gmail.com>
2023-01-17 13:50:51 +0100

    2016 EX1

--- src/main/java/A2016/Day1/Ex23.java
+++ src/main/java/A2016/Day1/Ex23.java
@@ -1,24 +1,22 @@
 package A2016.Day1;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
-
-public class Ex2 {
+public class Ex23 {
+    static char direction = 'H';
+    static String line;
+    static int distance;
+    static char tourne;
+    static String pos;
+    static boolean sortieBoucle = false;
+    //H B G D
+    static int[] tabDistance = new int[2];
+    static List<String> lstPos = new ArrayList<>();
+    static int distanceTT;
 
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("src/main/java/A2016/Day1/Chemin.txt"))) {
-            char direction = 'H';
-            String line;
-            int distance;
-            char tourne;
-            String pos;
-            //H B G D
-            int[] tabDistance = new int[4];
-            List<String> lstPos = new ArrayList<>();
-
-            int distanceTT;
 
             while ((line = br.readLine()) != null) {
                 String chaines[] = line.split(", ");
@@ -30,24 +28,20 @@ public class Ex2 {
                         switch (direction) {
                             case 'H':
                                 direction = 'D';
-                                tabDistance[3] += distance;
+                                isAlreadyGo(1,true);
                                 break;
-
                             case 'B':
                                 direction = 'G';
-                                tabDistance[2] += distance;
+                                isAlreadyGo(1,false);
                                 break;
-
                             case 'G':
                                 direction = 'H';
-                                tabDistance[0] += distance;
+                                isAlreadyGo(0,true);
                                 break;
-
                             case 'D':
                                 direction = 'B';
-                                tabDistance[1] += distance;
+                                isAlreadyGo(0,false);
                                 break;
-
                             default:
                                 throw new RuntimeException("IMPOSSIBLE DIRECTION " + tourne);
                         }
@@ -55,49 +49,51 @@ public class Ex2 {
                         switch (direction) {
                             case 'H':
                                 direction = 'G';
-                                tabDistance[2] += distance;
+                                isAlreadyGo(1,false);
                                 break;
-
                             case 'B':
                                 direction = 'D';
-                                tabDistance[3] += distance;
+                                isAlreadyGo(1,true);
                                 break;
-
                             case 'G':
                                 direction = 'B';
-                                tabDistance[1] += distance;
+                                isAlreadyGo(0,false);
                                 break;
-
                             case 'D':
                                 direction = 'H';
-                                tabDistance[0] += distance;
+                                isAlreadyGo(0,true);
                                 break;
-
                             default:
                                 throw new RuntimeException("IMPOSSIBLE DIRECTION " + tourne);
                         }
                     }
-
-                    pos = (tabDistance[0] - tabDistance[1]) + "-" + (tabDistance[2] - tabDistance[3]) ;
-                    System.out.println(pos);
-
-                    if(lstPos.indexOf(pos) != -1) {
-
-                        System.out.println("Distance TT: ");
+                    pos = tabDistance[0] + " - " + tabDistance[1] ;
+                    if(sortieBoucle) {
+                        System.out.println("pos TT: " + pos );
                         break;
                     };
-
                     lstPos.add(pos);
                 }
             }
-
-            distanceTT = Math.abs(tabDistance[0] - tabDistance[1]) + Math.abs(tabDistance[2] - tabDistance[3]);
-
+            distanceTT = Math.abs(tabDistance[0]) + Math.abs( tabDistance[1]);
             System.out.println("Distance TT: " + distanceTT);
-
         } catch (Exception e) {
             System.err.println("Erreur");
         }
-
+    }
+    private static void isAlreadyGo(int VertiHori, boolean ajout) {
+        for (int i = 0; i < distance; i++) {
+            if (ajout) {
+                tabDistance[VertiHori] ++;
+            } else {
+                tabDistance[VertiHori] --;
+            }
+            pos = tabDistance[0] + " - " + tabDistance[1] ;
+            if(lstPos.indexOf(pos) != -1) {
+                sortieBoucle = true;
+                break;
+            }
+            lstPos.add(pos);
+        }
     }
 }
