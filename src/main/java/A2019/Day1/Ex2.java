d5d9de7c65303420bb19519455b8b6dba2333b23
John <jona0610@gmail.com>
2023-01-31 15:12:35 +0100

    x

--- src/main/java/A2019/Day1/Ex2.java
+++ src/main/java/A2019/Day1/Ex2.java
@@ -1,14 +1,18 @@
 package A2019.Day1;
 import java.io.BufferedReader;
 import java.io.FileReader;
-
-public class Ex1 {
+public class Ex2 {
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new FileReader("src/main/java/A2019/Day1/input.txt"))) {
             String line;
             int total = 0;
+            int nb;
             while ((line = br.readLine()) != null) {
-                total += Integer.parseInt(line) / 3 - 2;
+                nb = Integer.parseInt(line) / 3 - 2;
+                while (nb>0) {
+                    total += nb;
+                    nb = nb / 3 - 2;
+                }
             }
             System.out.println(total);
         } catch (Exception e) {
